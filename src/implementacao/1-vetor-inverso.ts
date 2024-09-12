68a742bf0e67aa37a2419fdff2c93d8401e3ce0f
mylenaverspeelt <mylena49@hotmail.com>
2024-09-12 15:58:35 -0300

    feat: Resposta do Exercicio1

--- src/implementacao/1-vetor-inverso.ts
+++ src/implementacao/1-vetor-inverso.ts
@@ -1,4 +1,13 @@
-function apresentarVetorInverso(){
-    return null;
+function apresentarVetorInverso(vetor: number[]) {
+
+    let arrayInvertido: number[] = []
+
+    for (let i = vetor.length; i > 0; i--) {
+        arrayInvertido.push(i)
+    }
+    
+    console.log(arrayInvertido)
 
 }
+apresentarVetorInverso([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
