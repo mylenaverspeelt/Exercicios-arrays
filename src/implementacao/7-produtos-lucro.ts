315fc22cef7eb3d0e3d6caf49efd008d88cf5c80
mylenaverspeelt <mylena49@hotmail.com>
2024-09-13 15:41:23 -0300

    fix: Resposta do Exercicio7

--- src/implementacao/7-produtos-lucro.ts
+++ src/implementacao/7-produtos-lucro.ts
@@ -11,10 +11,13 @@ interface Produto {
   
     for (let i = 0; i < produtos.length; i++) {
       let lucro = produtos[i].valor - produtos[i].custo;
+
+      let porcentagem10 = produtos[i].custo * 0.10
+      let porcentagem30 =  produtos[i].custo * 0.30
   
-      if (lucro < 0.1) {
+      if (lucro < porcentagem10) {
         quantidadeMenorQue10++;
-      } else if (lucro >= 0.1 && lucro <= 0.3) {
+      } else if (lucro >= porcentagem10 && lucro <= porcentagem30) {
         quantidadeEntre10e30++;
       } else {
         quantidadeMaiorQue30++;
@@ -27,9 +30,26 @@ interface Produto {
   }
   
   calcularLucros([
-    { nome: "Detergente", custo: 2, valor: 2.5 },
-    { nome: "Sabão", custo: 4, valor: 4.1 },
-    { nome: "Sabão", custo: 4, valor: 4.25 },
-  
+    { nome: "Água Sanitária", custo: 3, valor: 4 },
+    { nome: "Lâmpada", custo: 12, valor: 16 },
+    { nome: "Fita Adesiva", custo: 1.5, valor: 2.5 },
+    { nome: "Copo Descartável", custo: 1.5, valor: 2.3 },
+    { nome: "Guardanapo", custo: 3, valor: 4.2 },
+    { nome: "Limpa Chão", custo: 6, valor: 8.2 },
+    { nome: "Esfregão", custo: 10, valor: 13.5 },
+    { nome: "Álcool 70%", custo: 4.5, valor: 6 },
+    { nome: "Cabo USB", custo: 6, valor: 8 },
+    { nome: "Lâmina de Barbear", custo: 2, valor: 3 },
+    { nome: "Escova de Cabelo", custo: 5, valor: 7 },
+    { nome: "Toalha de Mão", custo: 3, valor: 4.5 },
+    { nome: "Tira Manchas", custo: 7, valor: 9.5 },
+    { nome: "Limpador de Piso", custo: 6, valor: 8 },
+    { nome: "Removedor", custo: 4, valor: 5.5 },
+    { nome: "Esfregão Giratório", custo: 12, valor: 16.5 },
+    { nome: "Caixa Organizadora", custo: 8, valor: 11 },
+    { nome: "Bateria", custo: 3, valor: 4.2 },
+    { nome: "Creme para Rosto", custo: 10, valor: 13.5 },
+    { nome: "Aspirador de Pó", custo: 50, valor: 70 }
+
   ]);
   
