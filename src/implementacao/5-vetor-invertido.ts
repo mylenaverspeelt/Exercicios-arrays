2905a28cf9f96dd5202bc0fbe63034d285051126
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-13 13:28:40 -0300

    feat: Resposta do Exercicio5

--- src/implementacao/5-vetor-invertido.ts
+++ src/implementacao/5-vetor-invertido.ts
@@ -1,3 +1,10 @@
-function inverterVetor() {
-    return null;
+function inverterVetor(vetor: Array<number>) {
+    
+    let vetorInverso: number [] = []
+    
+    for(let i = vetor.length -1 ; i >= 0 ; i--){
+        vetorInverso.push(vetor[i])
+    }
+    console.log(vetorInverso)
 }
+inverterVetor([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30])
