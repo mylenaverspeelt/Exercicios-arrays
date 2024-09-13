437cd4ad57839e4760841747fbc3f2ce72c7ca4e
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-13 11:58:40 -0300

    feat: Resposta do Exercicio4

--- src/implementacao/4-intercalacao-vetores.ts
+++ src/implementacao/4-intercalacao-vetores.ts
@@ -1,3 +1,10 @@
-function separarPositivosNegativo() {
-    return null;
+function separarPositivosNegativo(vetor1: Array<number>, vetor2: Array<number>) {
+    let arrayIntercalado: number []=[]
+
+    for(let i = 0 ; i < vetor1.length ; i++){
+        arrayIntercalado.push(vetor1[i], vetor2[i])
+        
+    }
+    console.log(arrayIntercalado)
 }
+separarPositivosNegativo([1,2,3,4,5,6,7,8,9,10],[11,12,13,14,15,16,17,18,19,20])
