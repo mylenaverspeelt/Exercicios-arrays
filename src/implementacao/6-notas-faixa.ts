70ccb0799eb45316c58d5eabf588bd10bd5509db
mylenaverspeelt <mylena49@hotmail.com>
2024-09-13 14:00:46 -0300

    feat: Resposta Do Exercicio6

--- src/implementacao/6-notas-faixa.ts
+++ src/implementacao/6-notas-faixa.ts
@@ -1,3 +1,22 @@
-function contarNotasNaFaixa() {
-    return null;
+function contarNotasNaFaixa(vetor: Array<number>, media: number) {
+
+    let quantidadeAcimaDaMedia: number = 0
+    let quantidadeAbaixoDaMedia: number = 0
+
+    let porcentagem = media * 0.10
+
+    let dezAcimaDaMedia: number = media + porcentagem
+
+    for (let i = 0; i <= vetor.length; i++) {
+
+        if (vetor[i] > dezAcimaDaMedia) {
+            quantidadeAcimaDaMedia++
+        } else {
+            quantidadeAbaixoDaMedia++
+        }
+    }
+    console.log("Abaixo da media: " + quantidadeAbaixoDaMedia)
+    console.log("Acima da media: " + quantidadeAcimaDaMedia);
 }
+contarNotasNaFaixa([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 46, 47, 48, 49, 50], 7);
+
