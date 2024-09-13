3804bdd61108bac1d170b11dd364f35f6f32f519
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-13 11:29:05 -0300

    feat: Resposta do Exercicio3

--- src/implementacao/3-vetores-positivos-negativos.ts
+++ src/implementacao/3-vetores-positivos-negativos.ts
@@ -1,3 +1,16 @@
-function separarPositivosNegativos() {
-    return null;
+function separarPositivosNegativos(vetor: Array<number>) {
+    
+    let arrayPositivo: number [] = [];
+    let arrayNegativo: number [] = [];
+
+    for( let i = 0; i < vetor.length ; i++){
+        if( vetor[i] > 0 ){
+        arrayPositivo.push(vetor[i])
+        } else {
+            arrayNegativo.push(vetor[i])
+        }
+    }
+    console.log(arrayPositivo)
+    console.log(arrayNegativo)
 }
+separarPositivosNegativos([4,3,6,-3,-2,9,-4,7])
